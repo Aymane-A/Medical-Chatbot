76d511d520daadd92ee909ed6ac811eaf4c7bc0d
Aymane-A <khiaraymane07@hotmail.com>
2026-04-04 03:37:48 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Aymane KHIAR",
+    author_email="<khiaraymane07@hotmail.com>",
+    packages=find_packages(),
+    install_requires=[]
+)
+    
+    
+    
